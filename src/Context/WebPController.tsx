56e532c609ad3bbbd13b6b21823ec77918b72573
Alexander Letnikow <reclamme@gmail.com>
2019-06-18 06:24:10 +0300

    Refactor WebPController to use React Hooks

--- src/Context/WebPController.tsx
+++ src/Context/WebPController.tsx
@@ -1,30 +1,19 @@
 import * as React from "react";
 import { canUseWebP } from "../canUseWebP";
-import { WebP, WebPValue } from "./WebP";
+import { WebP } from "./WebP";
 
 export interface WebPControllerProps {
     accept: string | undefined;
 }
 
-export class WebPController extends React.Component<WebPControllerProps, WebPValue> {
-    public readonly state: WebPValue = {
-        supportWebP: this.supportWebpByHeaders,
-    }
-
-    public componentDidMount(): void {
-        const supportInRuntime = canUseWebP();
-        if (supportInRuntime && !this.state.supportWebP) {
-            this.setState({
-                supportWebP: true,
-            });
+export const WebPController: React.FC<WebPControllerProps> = ({ accept, children }) => {
+    const [ supportWebP, setSupport ] = React.useState<boolean>(
+        "string" === typeof accept && accept.indexOf("image/webp") !== -1
+    );
+    React.useEffect(() => {
+        if (!supportWebP && canUseWebP()) {
+            setSupport(true);
         }
-    }
-
-    public render() {
-        return <WebP.Provider value={this.state} children={this.props.children} />;
-    }
-
-    protected get supportWebpByHeaders(): boolean {
-        return !!this.props.accept && this.props.accept.indexOf("image/webp") !== -1;
-    }
-}
+    }, []);
+    return <WebP.Provider value={{ supportWebP }}>{children}</WebP.Provider>
+};
