61f03283c1cccbe2ea35986110e9376d223a35cf
Fernando Costa <ferco0@live.com>
2019-09-22 23:35:28 -0300

    Update canUseWebP.ts
    Remove unnecessary support check.
    
    Every call of module create a canvas element, the first call store the result for future calls.

--- src/canUseWebP.ts
+++ src/canUseWebP.ts
@@ -1,11 +1,18 @@
+let checked = undefined;
+
 export function canUseWebP(): boolean {
     if (typeof document !== "object") {
         return false;
     }
 
+    if(checked !== undefined)
+        return checked;
+    
     const canvas = document.createElement("canvas");
     if (!canvas.getContext || !canvas.getContext("2d")) {
+        checked = false;
         return false;
     }
-    return canvas.toDataURL("image/webp").indexOf("data:image/webp") === 0;
+    checked = canvas.toDataURL("image/webp").indexOf("data:image/webp") === 0
+    return checked;
 }
