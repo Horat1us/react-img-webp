af7bebc225c23f02963470130072a7e1017c2077
Fernando Costa <ferco0@live.com>
2019-10-02 20:19:37 -0300

    Update canUseWebP.ts

--- src/canUseWebP.ts
+++ src/canUseWebP.ts
@@ -1,13 +1,14 @@
-let checked: boolean;
+let checked: boolean; // eslint-disable-line
 
 export function canUseWebP(): boolean {
     if (typeof document !== "object") {
         return false;
     }
 
-    if(typeof checked !== 'undefined')
+    if (typeof checked !== 'undefined') {
         return checked;
-    
+    }
+
     const canvas = document.createElement("canvas");
     if (!canvas.getContext || !canvas.getContext("2d")) {
         checked = false;
