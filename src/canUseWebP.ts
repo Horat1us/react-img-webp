465fec127a674894cfafeb6a045fef7e1eac5655
Fernando Costa <ferco0@live.com>
2020-01-30 13:35:54 -0300

    Update canUseWebP.ts (#3)

--- src/canUseWebP.ts
+++ src/canUseWebP.ts
@@ -1,11 +1,19 @@
+let checked: boolean; // eslint-disable-line
+
 export function canUseWebP(): boolean {
     if (typeof document !== "object") {
         return false;
     }
 
+    if (typeof checked !== "undefined") {
+        return checked;
+    }
+
     const canvas = document.createElement("canvas");
     if (!canvas.getContext || !canvas.getContext("2d")) {
+        checked = false;
         return false;
     }
-    return canvas.toDataURL("image/webp").indexOf("data:image/webp") === 0;
+    checked = canvas.toDataURL("image/webp").indexOf("data:image/webp") === 0
+    return checked;
 }
